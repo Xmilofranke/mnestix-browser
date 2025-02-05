9305f8035414424e4ecd7ce23ca56225f51842cb
Milo Franke <milo.franke@hs-augsburg.de>
2025-02-05 22:47:52 +0100

    implement catalog list for fetched catalogs

---
+++ src/app/[locale]/dataspace/_components/CatalogList.tsx
@@ -0,0 +1,113 @@
+import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Divider, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const CONSUMER_CATALOG_QUERY_URL = 'http://localhost/consumer/fc';
+const CONNECTOR_API_KEY = 'password'; // The API Key of the own Connector
+
+export const CatalogList = async () => {
+    const catalogs: any[] = await fetchCatalogs();
+
+    async function fetchCatalogs() {
+        const jsonBody = {
+            '@context': ['https://w3id.org/edc/connector/management/v0.0.1'],
+            '@type': 'QuerySpec',
+        };
+
+        try {
+            const res = await fetch(CONSUMER_CATALOG_QUERY_URL + '/api/catalog/v1alpha/catalog/query', {
+                method: 'POST',
+                body: JSON.stringify(jsonBody),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Api-Key': CONNECTOR_API_KEY,
+                },
+            });
+            const json = await res.json();
+            console.log(json);
+            return json;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const renderCatalogs = (catalogs: any[]) => {
+        return (
+            <>
+                {catalogs
+                    .filter((catalog) => catalog['@type'] === 'dcat:Catalog')
+                    .map((catalog, i) => {
+                        return (
+                            <Accordion key={i}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1-content"
+                                    id={'' + i}
+                                >
+                                    <Box
+                                        sx={{
+                                            width: '100%',
+                                            display: 'flex',
+                                            alignItems: 'center',
+                                            justifyContent: 'space-between',
+                                            paddingY: '.5em',
+                                        }}
+                                    >
+                                        <Typography fontWeight={600}>{catalog['dspace:participantId']}</Typography>
+                                        <Typography>&nbsp;</Typography>
+                                        <Typography>{catalog['dcat:service']['dcat:endpointUrl']}</Typography>
+                                        <Typography>&nbsp;</Typography>
+                                    </Box>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    {renderDataset(catalog['dcat:dataset'])}
+                                    {renderCatalogs(catalog['dcat:catalog'])}
+                                </AccordionDetails>
+                            </Accordion>
+                        );
+                    })}
+            </>
+        );
+    };
+
+
+    const renderDataset = (dataset: any[]) => {
+        return (
+            <>
+                {Array.isArray(dataset) ? (
+                    <>
+                        {dataset.map((asset) => {
+                            return renderAsset(asset);
+                        })}
+                    </>
+                ) : (
+                    <>
+                        {renderAsset(dataset)}
+                    </>
+                )}
+            </>
+        );
+    };
+
+    const renderAsset = (asset: any) => {
+        return (
+            <>
+                <Divider />
+                <Box
+                    key={asset['@id']}
+                    sx={{
+                        width: '100%',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        paddingY: '.5em',
+                    }}
+                >
+                    <Typography>{asset['@id']}</Typography>
+                    <Button variant={'outlined'}>Initiate Negotiation</Button>
+                </Box>
+            </>
+        );
+    }
+
+    return <>{renderCatalogs(catalogs)}</>;
+};
--- src/app/[locale]/dataspace/page.tsx
+++ src/app/[locale]/dataspace/page.tsx
@@ -1,19 +1,21 @@
-'use client';
-
 import { Box } from '@mui/material';
 import { ViewHeading } from 'components/basics/ViewHeading';
-import { FormattedMessage } from 'react-intl';
-import { messages } from 'lib/i18n/localization';
+import { CatalogList } from 'app/[locale]/dataspace/_components/CatalogList';
+import { useTranslations } from 'next-intl';
 
 export default function Page() {
+    const t = useTranslations('dataspaces');
+
     return (
-        <Box sx={{ p:4, width: '100%', margin: '0 auto' }}>
+        <Box sx={{ p: 4, width: '100%', margin: '0 auto' }}>
             <Box sx={{ mb: 3 }}>
-                <ViewHeading title={<FormattedMessage {...messages.mnestix.dataspace} />}/>
+                <ViewHeading title={t('dataspace')} />
             </Box>
             <Box>
                 <p>Data Space Shenanigans start here!</p>
             </Box>
+
+            <CatalogList/>
         </Box>
     );
 }
