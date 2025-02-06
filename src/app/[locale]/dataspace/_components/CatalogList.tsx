335760ef2bf793a4ded4a8a430fc9e346a9978c9
Milo Franke <milo.franke@hs-augsburg.de>
2025-02-06 01:11:19 +0100

    move fetching catalogs to api file

--- src/app/[locale]/dataspace/_components/CatalogList.tsx
+++ src/app/[locale]/dataspace/_components/CatalogList.tsx
@@ -1,33 +1,9 @@
 import { Accordion, AccordionDetails, AccordionSummary, Box, Button, Divider, Typography } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-const CONSUMER_CATALOG_QUERY_URL = 'http://localhost/consumer/fc';
-const CONNECTOR_API_KEY = 'password'; // The API Key of the own Connector
+import { fetchCatalogs } from 'lib/api/dataspace-api';
 
 export const CatalogList = async () => {
     const catalogs: any[] = await fetchCatalogs();
-
-    async function fetchCatalogs() {
-        const jsonBody = {
-            '@context': ['https://w3id.org/edc/connector/management/v0.0.1'],
-            '@type': 'QuerySpec',
-        };
-
-        try {
-            const res = await fetch(CONSUMER_CATALOG_QUERY_URL + '/api/catalog/v1alpha/catalog/query', {
-                method: 'POST',
-                body: JSON.stringify(jsonBody),
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Api-Key': CONNECTOR_API_KEY,
-                },
-            });
-            const json = await res.json();
-            return json;
-        } catch (error) {
-            console.error(error);
-        }
-    }
 
     const renderCatalogs = (catalogs: any[]) => {
         return (
